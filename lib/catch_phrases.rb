c964adf5bf64cd41b7d1ccae93b85e7286e1b09e
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-07 22:31:27 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,9 +10,6 @@ def toadstool
   puts status
 end
 
-describe "any_phrase" do
-  it "takes in an argument and puts out the catch phrase" do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
-  end
+def any_phrase (phrase)
+puts phrase
 end
